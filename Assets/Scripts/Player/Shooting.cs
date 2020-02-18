8b837e62ac3ba5d3bad30cba0bab36bd3060a661
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2020-02-18 01:27:16 -0500

    a commitment of a prototype scene for player's movements and shooting

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform Muzzle;
+    public GameObject Airbullet;
+
+    public float AirBulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootAir();
+        }
+    }
+
+    void ShootAir()
+    {
+        GameObject bullet1 = Instantiate(Airbullet, Muzzle.position, Muzzle.rotation);
+        Rigidbody2D rb = bullet1.GetComponent<Rigidbody2D>();
+        rb.AddForce(Muzzle.up * AirBulletForce, ForceMode2D.Impulse);
+    }
+}
