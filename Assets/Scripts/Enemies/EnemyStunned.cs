f97e55a15efbbe4e86acc86736e4aaaa52c0f94e
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2020-02-24 02:34:34 -0500

    Changed the name of the Movement Prototype scene to White Box Prototype, which includes Air Cannon knocks back enemy and item pick ups for now

---
+++ Assets/Scripts/Enemies/EnemyStunned.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStunned : MonoBehaviour
+{
+    public Sprite Stunned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Air Bullet")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Stunned;
+
+            Vector2 difference = transform.position - other.transform.position;
+            transform.position = new Vector2(transform.position.x + difference.x, transform.position.y + difference.y);
+
+            Debug.Log("Hit");
+        }
+    }
+}
