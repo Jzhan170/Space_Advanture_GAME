75df61c306a8513c49b0a3aea6569b80a41086b4
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2020-03-02 02:42:58 -0500

    Touch Control and tap to shoot, also adjust the collider around the border of the environment so the player won't move beyond

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -11,9 +11,13 @@ public class Player_Movement : MonoBehaviour
     public Camera cam;
 
     Vector2 movement;
-    Vector2 mousePos;
+    Vector2 touchPos;
+
+    public Joystick joystick;
 
     public VectorValues startingPosition;
+
+    
 
     void Start()
     {
@@ -22,17 +26,24 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+       
+    
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
+    
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPos = cam.ScreenToWorldPoint(touch.position);
+        }
+        //touchPos = cam.ScreenToWorldPoint(touch.position);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDir = mousePos - rb.position;
+        Vector2 lookDir = touchPos - rb.position;
         float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
     }
---
+++ Assets/Scripts/Player/joystick_Shoot.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joystick_Shoot : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5f;
+    private Vector2 startingPoint;
+    private int leftTouch = 99;
+    public Joystick movement_joystick;
+
+    public GameObject Airbullet;
+    public Transform Muzzle;
+
+    public float AirBulletForce = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            Touch t = Input.GetTouch(i);
+            Vector2 touchPos = getTouchPosition(t.position) * -1;
+            if(t.phase == TouchPhase.Began)
+            {
+                if(t.position.x > Screen.width / 2)
+                {
+                    shootBullet();
+                }
+                else
+                {
+                    leftTouch = t.fingerId;
+                    startingPoint = touchPos;
+                }
+            }else if(t.phase == TouchPhase.Moved && leftTouch == t.fingerId)
+            {
+                Vector2 offset = touchPos - startingPoint;
+                Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+                moveCharacter(direction);
+            }else if(t.phase == TouchPhase.Ended && leftTouch == t.fingerId)
+            {
+                leftTouch = 99;
+            }
+            ++i;
+        }
+    }
+    Vector2 getTouchPosition(Vector2 touchPosition)
+    {
+        return GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, transform.position.z));
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        //player.Translate(direction * speed * Time.deltaTime);
+    }
+    void shootBullet()
+    {
+        GameObject bullet1 = Instantiate(Airbullet, Muzzle.position, Muzzle.rotation);
+        Rigidbody2D rb = bullet1.GetComponent<Rigidbody2D>();
+        rb.AddForce(Muzzle.up * AirBulletForce, ForceMode2D.Impulse);
+
+        Destroy(bullet1.gameObject, 0.1f);
+    }
+}
